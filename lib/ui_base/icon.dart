721c6785810aa68701eb2de134d50e2a2c96ac0f
“LHM” <lhm@example.com>
2024-07-04 11:34:39 +0800

    fix

--- lib/ui_base/icon.dart
+++ lib/ui_base/icon.dart
@@ -1,9 +1,4 @@
-import 'dart:math';
-
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class IconDemo extends StatelessWidget {
--- lib/ui_base/image.dart
+++ lib/ui_base/image.dart
@@ -1,42 +1,36 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:photo_view/photo_view.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class ImageDemo extends StatelessWidget {
   const ImageDemo({super.key});
 
-  final img = const AssetImage("assets/images/hello2.png");
+  final img = const NetworkImage(
+      "https://raw.githubusercontent.com/nsnans/picGo-bed/main/vitepress_1x/202407021130232.png");
 
   List<Widget> _buildNetwork() {
     return [
-      const Text("加载图片"),
+      const Text("普通加载网络图片"),
       Image.network(
-        "https://picsum.photos/250?image=9",
-        width: 100.0,
+        "https://raw.githubusercontent.com/nsnans/picGo-bed/main/vitepress_1x/202407021130232.png",
       ),
-      Container(
-        height: 100,
-        child: PhotoView(
-          imageProvider: const AssetImage(
-            "assets/images/hello2.png",
-          ),
-        ),
-      ),
+      const Text("普通加载本地图片"),
       Image.asset(
         "assets/images/hello2.png",
-        width: 100.0,
-        height: 100,
       ),
       //占位图和淡入效果
+      const Text("加载网络图片:占位图和淡入效果"),
       FadeInImage.memoryNetwork(
         placeholder: kTransparentImage,
-        image: 'https://picsum.photos/250?image=9',
+        image:
+            'https://raw.githubusercontent.com/nsnans/picGo-bed/main/vitepress_1x/202407021130232.png',
       ),
+      const Text("加载网络图片:缓存网络图片"),
       //cachednetworkimage组件的缓存系统可以将图片缓存到内存和磁盘，并基于使用模式自动清理过期的缓存条目，
       CachedNetworkImage(
         placeholder: (context, url) => const CircularProgressIndicator(),
-        imageUrl: 'https://picsum.photos/250?image=9',
+        imageUrl:
+            'https://raw.githubusercontent.com/nsnans/picGo-bed/main/vitepress_1x/202407021130232.png',
       ),
     ];
   }
@@ -45,49 +39,50 @@ class ImageDemo extends StatelessWidget {
     var widgets = [
       Image(
         image: img,
-        height: 50.0,
-        width: 100.0,
+        height: 400,
+        width: 40,
         fit: BoxFit.fill,
       ),
       Image(
         image: img,
-        height: 50,
-        width: 50.0,
+        height: 400,
+        width: 40,
         fit: BoxFit.contain,
       ),
       Image(
         image: img,
-        width: 100.0,
-        height: 50.0,
+        height: 400,
+        width: 20,
         fit: BoxFit.cover,
       ),
       Image(
         image: img,
-        width: 100.0,
-        height: 50.0,
+        height: 400,
+        width: 20,
         fit: BoxFit.fitWidth,
       ),
       Image(
         image: img,
-        width: 100.0,
-        height: 50.0,
+        height: 400,
+        width: 20,
         fit: BoxFit.fitHeight,
       ),
       Image(
         image: img,
-        width: 100.0,
-        height: 50.0,
+        height: 400,
+        width: 20,
         fit: BoxFit.scaleDown,
       ),
       Image(
         image: img,
-        height: 50.0,
-        width: 100.0,
+        height: 400,
+        width: 20,
         fit: BoxFit.none,
       ),
       Image(
         image: img,
-        width: 100.0,
+        height: 400,
+        width: 200,
         color: Colors.blue,
         //颜色反色，高对比度
         colorBlendMode: BlendMode.difference,
@@ -95,40 +90,43 @@ class ImageDemo extends StatelessWidget {
       ),
       Image(
         image: img,
-        width: 100.0,
-        height: 200.0,
+        height: 400,
+        width: 200,
         repeat: ImageRepeat.repeatY,
       )
-    ].map((e) {
-      return Row(
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-                border: Border.all(width: 1, color: Colors.black)),
-            margin: const EdgeInsets.all(16.0),
-            child: SizedBox(
-              width: 100,
-              child: e,
+    ].map(
+      (e) {
+        return Row(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: Colors.black)),
+              margin: const EdgeInsets.all(16.0),
+              child: SizedBox(
+                width: 100,
+                child: e,
+              ),
             ),
-          ),
-          Text(e.fit.toString())
-        ],
-      );
-    }).toList();
-    return [const Text("填充图片"), ...widgets];
+            Text(e.fit != null ? e.fit.toString() : '直接使用')
+          ],
+        );
+      },
+    ).toList();
+    return [...widgets];
   }
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-        child: SingleChildScrollView(
-      physics: const AlwaysScrollableScrollPhysics(),
-      child: Column(
-        children: [
-          ..._buildNetwork(),
-          ..._buildImageFill(),
-        ],
+      child: SingleChildScrollView(
+        physics: const AlwaysScrollableScrollPhysics(),
+        child: Column(
+          children: [
+            ..._buildNetwork(),
+            ..._buildImageFill(),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
